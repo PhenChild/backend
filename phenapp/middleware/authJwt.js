2a262119bcdf8b6cfc8b39f002c9e168330e0f16
lzambranoz <lzambranoz@armada.mil.ec>
2021-06-01 01:59:39 -0500

    middlewares para autenticacion

---
+++ phenapp/middleware/authJwt.js
@@ -0,0 +1,56 @@
+const jwt = require("jsonwebtoken");
+const config = require("../config/auth.config.js");
+const db = require("../models");
+const User = db.User;
+
+verifyToken = (req, res, next) => {
+  let token = req.headers["x-access-token"];
+
+  if (!token) {
+    return res.status(403).send({
+      message: "No token provided!"
+    });
+  }
+
+  jwt.verify(token, config.secret, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({
+        message: "Unauthorized!"
+      });
+    }
+    req.userId = decoded.id;
+    next();
+  });
+};
+
+isAdmin = (req, res, next) => {
+  User.findByPk(req.userId).then(user => {
+    if (user.role === "admin") {
+      next();
+      return;
+    }
+    res.status(403).send({
+      message: "Require Admin Role!"
+    });
+    return;
+  });
+};
+
+isObserver = (req, res, next) => {
+  User.findByPk(req.userId).then(user => {
+    if (user.role === "observer") {
+      next();
+      return;
+    }
+    res.status(403).send({
+      message: "Require Observer Role!"
+    });
+  });
+};
+
+const authJwt = {
+  verifyToken: verifyToken,
+  isAdmin: isAdmin,
+  isObserver: isObserver
+};
+module.exports = authJwt;
---
+++ phenapp/middleware/verifySignUp.js
@@ -0,0 +1,39 @@
+const db = require("../models");
+const ROLES = require('../constants/ENUM').ROLES;
+const User = db.User;
+
+checkDuplicateEmail = (req, res, next) => {
+  // Username
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(user => {
+    if (user) {
+      res.status(400).send({
+        message: "Failed! Email is already in use!"
+      });
+      return;
+    }
+    next();
+  });
+};
+
+checkRoleExisted = (req, res, next) => {
+  if (req.body.role) {
+    if (!ROLES.includes(req.body.role)) {
+      res.status(400).send({
+        message: "Failed! Role does not exist = " + req.body.role
+      });
+      return;
+    }
+  }
+  next();
+};
+
+const verifySignUp = {
+  checkDuplicateEmail: checkDuplicateEmail,
+  checkRoleExisted: checkRoleExisted
+};
+
+module.exports = verifySignUp;
