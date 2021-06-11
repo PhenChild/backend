3144b60ddb0b5e0756bac85614aa406aa4dd4353
MichArceS <60787304+MichArceS@users.noreply.github.com>
2021-06-11 15:51:07 -0500

    Update file.sql

--- phenapp/file.sql
+++ phenapp/file.sql
@@ -2,21 +2,34 @@ INSERT INTO public."Estacion"
 ("codigoEstacion", "nombreEstacion", posicion, altitud, suelo, omm, "createdAt", "updatedAt")
 values
 ('EST001', 'Estacion Esmeraldas',point(-2.15,80.05)::geometry, 500, 'suelo', 'oom', pg_catalog.now() , pg_catalog.now()),
-('EST002', 'Estacion Manabi',point(-2.02,79.1)::geometry, 500, 'suelo', 'oom', pg_catalog.now() , pg_catalog.now());
+('EST002', 'Estacion Manabi',point(-2.02,79.1)::geometry, 500, 'suelo', 'oom', pg_catalog.now() , pg_catalog.now()),
+('EST1064','El Reloj',point(-19.03806,-65.24306)::geometry,2840,'suelo','omm', pg_catalog.now() , pg_catalog.now()),
+('EST1003','Azurduy',point(-20.10056,-64.41)::geometry,2530,'suelo','omm', pg_catalog.now() , pg_catalog.now()),
+('EST1005','Cachimayu',point(-19.13333,-65.26667)::geometry,2400,'suelo','omm', pg_catalog.now() , pg_catalog.now()),
+('EST8022','Baures Aeropuerto',point(-13.65806,-63.70194)::geometry,150,'suelo','omm', pg_catalog.now() , pg_catalog.now()),
+('EST9002','Cobija Aeropuerto',point(-11.03972,-68.78028)::geometry,235,'suelo','omm', pg_catalog.now() , pg_catalog.now());
 
 INSERT INTO public."Horario"
 ("tipoHora", hora, "createdAt", "updatedAt")
 VALUES
-('diario'::"enum_Horario_tipoHora", '7:00', pg_catalog.now(), pg_catalog.now()),
-('diario'::"enum_Horario_tipoHora", '13:00', pg_catalog.now(), pg_catalog.now()),
-('diario'::"enum_Horario_tipoHora", '19:00', pg_catalog.now(), pg_catalog.now()),
-('parcial'::"enum_Horario_tipoHora", '00:00', pg_catalog.now(), pg_catalog.now());
+('parcial'::"enum_Horario_tipoHora", '08:00', pg_catalog.now(), pg_catalog.now()),
+('parcial'::"enum_Horario_tipoHora", '14:00', pg_catalog.now(), pg_catalog.now()),
+('parcial'::"enum_Horario_tipoHora", '18:00', pg_catalog.now(), pg_catalog.now()),
+('diario'::"enum_Horario_tipoHora", '16:00', pg_catalog.now(), pg_catalog.now());
 
 INSERT INTO public."Variable"
 (nombre, unidad, maximo, minimo, "tipoDato", "createdAt", "updatedAt")
 VALUES
-('Temperatura', 'Celcius', 0, 100, 'Float', pg_catalog.now(),pg_catalog.now() ),
-('Humedad Relativa', 'Porcentaje', 0, 100, 'Float', pg_catalog.now(),pg_catalog.now() );
+('Temperatura','C',60,-60,'float',pg_catalog.now(),pg_catalog.now()),
+('Velocidad del Viento','m/s',0,107,'float',pg_catalog.now(),pg_catalog.now()),
+('Direccion del Viento','Rosa',NULL,NULL,'entero',pg_catalog.now(),pg_catalog.now()),
+('Humedad Relativa','%',100,0,'float',pg_catalog.now(),pg_catalog.now()),
+('Temperatura Bulbo Seco','C',60,-60,'float',pg_catalog.now(),pg_catalog.now()),
+('Temperatura Bulbo Humedo','C',60,-60,'float',pg_catalog.now(),pg_catalog.now()),
+('Temperatura maxima','C',60,NULL,'float',pg_catalog.now(),pg_catalog.now()),
+('Temperatura minima','C',NULL,-60,'float',pg_catalog.now(),pg_catalog.now()),
+('Precipitacion','mm',NULL,0,'float',pg_catalog.now(),pg_catalog.now()),
+('Presion atmosferica','hPa',1100,800,'float',pg_catalog.now(),pg_catalog.now());
 
 INSERT INTO public."Instrumento"
 (codigo, nombre, "createdAt", "updatedAt", "EstacionCodigo")
